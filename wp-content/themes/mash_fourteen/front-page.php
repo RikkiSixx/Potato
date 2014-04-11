cd0fb14a3a8c1524789a1a35b1ec39984195ef42
Rik Kendell <rik.kendell@gmail.com>
2014-04-11 15:28:50 +0100

    Homepage - Removed .grid

--- wp-content/themes/mash_fourteen/front-page.php
+++ wp-content/themes/mash_fourteen/front-page.php
@@ -12,7 +12,7 @@
 		if ( have_posts() ) : while ( have_posts() ) : the_post();
 	?>
 
-	<article class="project-item">		
+	<article class="project-item desk-one-third lap-one-half">		
 
 		<section class="entry-content">
 			<header class="header">
@@ -21,13 +21,11 @@
 				</h1>
 			</header>
 
-			<a href="<?php the_permalink(); ?>">
-				<?php if ( has_post_thumbnail() ) { ?>
-					<?php the_post_thumbnail('project-sm'); ?></a>
-				<?php } else { ?>
-					<img src="<?php echo get_template_directory_uri(); ?>/img/placeholder-sm.jpg" width="100%" height="auto" />
-				<?php } ?>
-			</a>
+			<?php if ( has_post_thumbnail() ) { ?>
+				<?php the_post_thumbnail('project-sm'); ?>
+			<?php } else { ?>
+				<img src="<?php echo get_template_directory_uri(); ?>/img/placeholder-sm.jpg" width="100%" height="auto" />
+			<?php } ?>
 
 			<?php // the_content(); ?>
 		</section>
