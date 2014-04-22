7fc3b0d8b25c78465adb0d1348232bf60c6b2576
Rik Kendell <rik.kendell@gmail.com>
2014-04-22 20:55:21 +0100

    Post images

--- wp-content/themes/mash_fourteen/entry-content.php
+++ wp-content/themes/mash_fourteen/entry-content.php
@@ -1,12 +1,8 @@
 <section class="entry-content post-wrap cf">
 
-	<div class="lap-one-third desk-two-thirds post-image">
-		<?php if ( has_post_thumbnail() ) { the_post_thumbnail('project-lg'); } ?>
-	</div>
+	<?php if ( has_post_thumbnail() ) { the_post_thumbnail('project-lg'); } ?>
 
-	<div class="lap-two-thirds desk-one-third post-content">
-		<?php the_content(); ?>
-	</div>
+	<?php the_content(); ?>
 
 	<div class="entry-links"><?php wp_link_pages(); ?></div>
 </section>
