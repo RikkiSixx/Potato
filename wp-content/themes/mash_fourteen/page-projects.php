9427300c9b26261624b30f9d65bfd3c8a389dea7
Rik Kendell <rik.kendell@gmail.com>
2014-08-09 17:57:32 +0100

    Merge remote-tracking branch 'origin/master'
    Grid layout

--- wp-content/themes/mash_fourteen/page-projects.php
+++ wp-content/themes/mash_fourteen/page-projects.php
@@ -40,39 +40,67 @@ Template Name: Projects List
 
 	<section role="main" class="project-grid cf">
 
-		<?php                  
-	        $args = array(
-	            'post_type' => 'project',
-	            'posts_per_page' => 9
-	        );
-	        query_posts( $args );
-	    ?>
-		
-		<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
-
-		<article class="project-item desk-one-third lap-one-half">		
-
+		<?php
+	// The third image should be a large featured image
+	$count = 1;
+	
+	// Find posts in 'Projects' post type 
+	$page = (get_query_var('p')) ? get_query_var('p') : 1;  
+	$args = array(
+		'posts_per_page' => 12,
+		'post_type' => 'project',
+		'paged' => 1
+	);
+	query_posts($args);
+	
+	// Get latest featured post
+	$arr_featured = getFeaturedProjects(2);
+	
+	// Grid position of featured items [current item index => featured arr pos]
+	$featured_pos = array(
+		2 => 0,
+		7 => 1
+	);
+			
+	if ( have_posts() ) : while ( have_posts() ) : the_post(); 
+		$size = (in_array($count, array_keys($featured_pos)) && isset($arr_featured[$featured_pos[$count]])) ? 'lg' : 'sm';
+		if($size == 'lg') {
+			$class = 'desk-two-thirds';
+		} else {
+			$class = 'desk-one-third';
+		}
+		?>
+		<article class="project-item <?php echo $class; ?> lap-one-half">		
 			<section class="entry-content">
 				<header class="header">
 					<h3 class="entry-title">
-						<a href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
+						<a href="<?php the_permalink(); ?>"><span><?php the_title(); ?></span></a>
 					</h3>
+					<?php the_category(); ?>
 				</header>
-
-				<?php if ( has_post_thumbnail() ) { ?>
-					<?php the_post_thumbnail('project-sm'); ?>
-				<?php } else { ?>
-					<img src="<?php echo get_template_directory_uri(); ?>/img/placeholder-sm.jpg" width="100%" height="auto" />
-				<?php } ?>
-
-				<?php // the_content(); ?>
+				<?php // Featured project
+				if($size == 'lg'):
+					setup_postdata($arr_featured[$featured_pos[$count]]);
+					the_post_thumbnail('project-lg');
+					wp_reset_postdata();
+				else:
+					// Standard projects
+					if ( has_post_thumbnail() ) { ?>
+						<?php the_post_thumbnail('project-sm'); ?>
+					<?php } else { ?>
+						<img src="<?php echo get_template_directory_uri(); ?>/img/placeholder-sm.jpg" width="100%" height="auto" />
+					<?php } 
+				endif; ?>
 			</section>
-
 		</article>
-
-		<?php endwhile; endif; ?>
-
-		<?php wp_reset_query(); ?>
+		<?php $count++;
+	endwhile; endif; ?>
+	
+	<div style="position:relative; top:-33px">
+		<div class="nav-previous"><a href="/?p=2">More</a></div>
+	</div>
+	
+	<?php wp_reset_query(); ?>
 
 	</section>
 
