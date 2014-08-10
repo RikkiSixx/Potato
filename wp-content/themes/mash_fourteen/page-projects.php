9aebce21911ed916c1d0bc36e51eaf3f88f03b03
Rik Kendell <rik.kendell@gmail.com>
2014-08-10 22:33:06 +0100

    Number of items on Project page

--- wp-content/themes/mash_fourteen/page-projects.php
+++ wp-content/themes/mash_fourteen/page-projects.php
@@ -47,7 +47,7 @@ Template Name: Projects List
 	// Find posts in 'Projects' post type 
 	$page = (get_query_var('p')) ? get_query_var('p') : 1;  
 	$args = array(
-		'posts_per_page' => 6,
+		'posts_per_page' => 9,
 		'post_type' => 'project',
 		'paged' => 1
 	);
