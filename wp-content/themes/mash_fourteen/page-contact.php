aa676113b81be04200717af051dc1e74daa5b577
Rik Kendell <rik.kendell@gmail.com>
2014-08-21 17:23:58 +0100

    Re-building Contact page. Styling Services icons

--- wp-content/themes/mash_fourteen/page-contact.php
+++ wp-content/themes/mash_fourteen/page-contact.php
@@ -10,34 +10,40 @@ Template Name: Contact
 	
 	<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
 
-	<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+	<article <?php post_class(); ?>>
 		<header class="header">
-			<h1 class="entry-title"><?php the_title(); ?></h1> <?php edit_post_link(); ?>
+			<h1 class="entry-title"><?php the_title(); ?></h1>
 		</header>
 
-		<div class="cf grid">
-			<section class="entry-content map-canvas lap-and-up-two-thirds grid__item">
-				<iframe width="100%" height="450" frameborder="0" style="border:0" src="https://www.google.com/maps/embed/v1/place?q=London%20EC2A3AR%2C%20United%20Kingdom&key=AIzaSyDawMmrxNHx29kIGQTzZKElj-uE_8Jq02A"></iframe>
-
-				<?php if ( has_post_thumbnail() ) { the_post_thumbnail(); } ?>
-
-				<?php the_content(); ?>
-
-				<div class="entry-links">
-					<?php wp_link_pages(); ?>
+		<div class="entry-content grid">
+			<div class="row cf">
+				<div class="intro lap-and-up-two-thirds grid__item">
+					<?php the_content(); ?>
 				</div>
-			</section>
 
-			<aside class="address lap-and-up-one-third grid__item">
-				<?php $options = get_option('mash_fourteen_theme_options'); ?>
+				<aside class="address lap-and-up-one-third grid__item">
+					<?php $options = get_option('mash_fourteen_theme_options'); ?>
 
-				<?php 
-					echo '<p>'.($options["address_line_1"]).'<br />'.($options["address_line_2"]).'<br />'.($options["address_line_3"]).'</p>';
-					echo '<p>'.($options["phone_number"]).'</p>';
-					echo '<p><a href='.($options["contact_email"]).'>'.($options["contact_email"]).'</a></p>'; 
-				?>			
-			</aside>
-		</div>
+					<?php 
+						echo '<p>'.($options["address_line_1"]).'<br />'.($options["address_line_2"]).'<br />'.($options["address_line_3"]).'</p>';
+						echo '<p>'.($options["phone_number"]).'</p>';
+						echo '<p><a href='.($options["contact_email"]).'>'.($options["contact_email"]).'</a></p>'; 
+					?>			
+				</aside>
+			</div><!-- .row -->
+
+			<div class="row cf">
+				<div class=" map-canvas lap-and-up-two-thirds grid__item">
+					<iframe width="100%" height="450" frameborder="0" style="border:0" src="https://www.google.com/maps/embed/v1/place?q=London%20EC2A3AR%2C%20United%20Kingdom&key=AIzaSyDawMmrxNHx29kIGQTzZKElj-uE_8Jq02A"></iframe>
+				</div>
+
+				<div class="contact-imgs lap-and-up-one-third grid__item">
+					<img src="<?php echo get_template_directory_uri(); ?>/img/studio-1.jpg" title="Studio" />
+					<img src="<?php echo get_template_directory_uri(); ?>/img/studio-1.jpg" title="Studio" />
+				</div>
+			</div><!-- .row -->
+			
+		</div><!-- .entry-content -->
 	</article><!-- .grid -->
 
 	<?php endwhile; endif; ?>
@@ -47,9 +53,6 @@ Template Name: Contact
 
 <section class="cf">
 
-	<div class="map">
-		MAP
-	</div>
 
 
 	
